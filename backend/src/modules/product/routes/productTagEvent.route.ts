8479736e0b68a444d2401f9439f5954d14e1bf06
Edward_Bacara <tutorship57@gmail.com>
2026-01-17 20:03:13 +0700

    feat:create-productTagEvent-routes

---
+++ backend/src/modules/product/routes/productTagEvent.route.ts
@@ -0,0 +1,14 @@
+import { createProductTagEvent, getAllProductTagEvents, getProductTagEventsByProductId, updateProductTagEvent, deleteProductTagEvent } from "../controller/productTagEvent.controller";
+
+
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", getAllProductTagEvents);
+router.get("/:tag_id", getProductTagEventsByProductId);
+router.post("/", createProductTagEvent);
+router.put("/", updateProductTagEvent);
+router.delete("/:tag_id", deleteProductTagEvent);
+
+export default router;
