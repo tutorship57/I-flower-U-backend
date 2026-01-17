ee89101b68973494e57be4d738b4bc378b8b769f
Edward_Bacara <tutorship57@gmail.com>
2026-01-17 19:06:54 +0700

    fix:make-change drop column tag_name on productTagEvent

---
+++ backend/prisma/migrations/20260117115957_delete_product_tag_event_tag_name_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tag_name` on the `ProductTagEvent` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ProductTagEvent" DROP COLUMN "tag_name";
