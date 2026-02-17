a2275d2fefa32a8bda7ce78482bff57b8f9fa00c
Edward_Bacara <tutorship57@gmail.com>
2026-02-17 19:51:08 +0700

    fix(guard):change ownershipGuard from middleware factory to plain middleware pattern

--- backend/src/modules/cart/routes/cart.route.ts
+++ backend/src/modules/cart/routes/cart.route.ts
@@ -3,13 +3,14 @@ import { Router } from "express";
 import cartItemRouter from './cartItem.route'
 import { sessionAuth } from "../../../shared/guards/session.guard";
 import { requireRole } from "../../../shared/guards/role.guard";
-import { userResourceOwnershipGuard } from "../../../shared/guards/ownership.guard";
+import { cartResourceOwnershipGuard, userResourceOwnershipGuard } from "../../../shared/guards/ownership.guard";
 const router = Router();
 
 router.get('/:user_id',sessionAuth,requireRole('user'),userResourceOwnershipGuard,getCartByUserId);
 router.post('/', createCart);
 // router.delete('/:user_id',sessionAuth,requireRole('user'),ownershipGuard,deleteCart);
-router.use('/:cart_id/cart-item',sessionAuth,requireRole('user'),userResourceOwnershipGuard,cartItemRouter);
+router.use('/:cart_id/cart-item',sessionAuth,requireRole('user'),cartResourceOwnershipGuard,cartItemRouter);
+
 
 
 export default router;
--- backend/src/shared/guards/ownership.guard.ts
+++ backend/src/shared/guards/ownership.guard.ts
@@ -1,12 +1,10 @@
 import type { Request, Response, NextFunction } from 'express';
 import { AppError } from '../utils/appErrorCustomize.util';
 import { getProductOwnerShipService } from '../../modules/product/service/product.service';
+import { getCartByUserId } from '../../modules/cart/controller/cart.controller';
+import { getCartByUserIdService } from '../../modules/cart/service/cart.service';
 
-
-export const userResourceOwnershipGuard = () => {
-  return (req: Request, res: Response, next: NextFunction) => {
-
-
+export const userResourceOwnershipGuard =  (req: Request, res: Response, next: NextFunction) => {
     const userRole = req.session?.user_id;
     const searchedUserId = req.params.user_id;
     
@@ -17,10 +15,9 @@ export const userResourceOwnershipGuard = () => {
     }
     next();
   };
-};
 
-export const productResourceOwnershipGuard =() => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+
+export const productResourceOwnershipGuard = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userRole = req.session?.user_role;
       const userId = req.session?.user_id;
@@ -39,4 +36,20 @@ export const productResourceOwnershipGuard =() => {
       next(err);
     }
   };
+
+export const cartResourceOwnershipGuard =  async (req: Request, res: Response, next: NextFunction) => {
+    const userRole = req.session?.user_id;
+    const userId = req.session?.user_id;
+    const cartIdQueryParam = req.params.cart_id;
+
+    const cart = await getCartByUserIdService(userId)
+    const cartIdFromUserID = cart.cart_id ;
+
+    if (userRole === 'admin') return next();
+
+    if (cartIdQueryParam !== cartIdFromUserID) {
+      return next(new AppError("Forbidden: insufficient permissions", 403));
+    }
+    next();
+  
 };
