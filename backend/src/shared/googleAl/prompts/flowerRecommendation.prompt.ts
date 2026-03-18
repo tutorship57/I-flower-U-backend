ecbe1d5ab93f01d7e0f5f641b743b7c22d5945f0
Edward_Bacara <tutorship57@gmail.com>
2026-03-18 23:26:19 +0700

    fix:add statement prompt

--- backend/src/shared/googleAl/prompts/flowerRecommendation.prompt.ts
+++ backend/src/shared/googleAl/prompts/flowerRecommendation.prompt.ts
@@ -14,7 +14,9 @@ const generatePrompt = (flowerName:string[], userInput: string)=>{
         You are a flower recommendation assistant. 
         Based on the user input below,
         choose the top 3 suitable flowers for that user description. 
-        translate the description base on the user input 
+        translate the description base on the user input language
+
+        this is the flower database: ${JSON.stringify(flowerName)}
         
         User input: ${userInput}
 
@@ -22,16 +24,18 @@ const generatePrompt = (flowerName:string[], userInput: string)=>{
         - "user_personality": a description of the user 
         - "suitable_flowers": an array of up to 3 flowers in English
         - "flower_description": a brief description for each recommended flower explaining why it is suitable for the user's personality in .
+        - "reason" : a brief description for each recommended flower explaining why it is suitable for the user's personality in English
         output example:
         {
           "user_personality": "A romantic and thoughtful individual who values meaningful gestures and has a deep appreciation for nature's beauty.",
           "suitable_flowers": ["Roses", "Tulips", "Lilies"]
-          "flower_description": {
-              ["Roses": "Symbolize love and passion, perfect for expressing deep emotions.(in Thai Language)"],
-              ["Tulips": "Represent elegance and grace, ideal for a sophisticated touch.(in Thai Language)"],
-              ["Lilies": "Convey purity and refined beauty, great for a thoughtful gesture.(in Thai Language)"]
-          }
+          "flower_description": [
+              "Roses": "Symbolize love and passion, perfect for expressing deep emotions.",
+              "Tulips": "Represent elegance and grace, ideal for a sophisticated touch.",
+              "Lilies": "Convey purity and refined beauty, great for a thoughtful gesture."
+            ]"     
         }
+        The response must be strictly valid JSON.
         `;
     return prompt2;
 }
