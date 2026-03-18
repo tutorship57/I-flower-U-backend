ad2d6f806ce49508cadf8d613bd7748d8833493b
Edward_Bacara <tutorship57@gmail.com>
2026-03-18 22:54:27 +0700

    fix:change number of user.seed

--- backend/prisma/seeds/user.seed.ts
+++ backend/prisma/seeds/user.seed.ts
@@ -4,19 +4,19 @@ let users = [{
     user_name: "admin",
     user_email: "tutor@example.com",
     user_password: "securepassword123",
-    role_id: 1
+    role_id: 3
     },
     {
     user_name: "user",
     user_email: "user@example.com",
     user_password: "securepassword123",
-    role_id: 2
+    role_id: 1
     },
     {
     user_name: "seller",
     user_email: "seller@example.com",
     user_password: "securepassword123",
-    role_id: 3
+    role_id: 2
     }
 ]
 
